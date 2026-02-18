0f1f0c25474060ef0e7733683b10fad444a500c8
Jalen M <119905831+JalenLikes2Code@users.noreply.github.com>
2026-02-18 15:46:40 -0600

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -6,3 +6,19 @@ function toggleSection(header) {
     next.style.display = "none";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggles = document.querySelectorAll(".collapse-toggle");
+
+  toggles.forEach(button => {
+    button.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  });
+});
